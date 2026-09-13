86beba1e2b35c7370931c040c3b1891852e114b9
Riad <riadk910@gmail.com>
2026-09-13 19:59:21 +0600

    Icon added

--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
 def header_home():
-    st.header('CSE Classroom')
+    
+    logo_url = 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Jstu_logo.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original'
+    st.markdown(f"""
+        <div>
+        <img src ='{logo_url}' style='height:100px';/>
+        
+    </div>
+                
+                """, unsafe_allow_html=True)
